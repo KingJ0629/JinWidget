0289fd4baa7b4077a19fabe5794bb715e8208ccf
金锋 <jinfeng@uama.com.cn>
2016-07-20 09:46:45 +0800

    success again

---
+++ custom-view/src/main/java/com/hangzhou/jin/customview/TailsTextView.java
@@ -0,0 +1,58 @@
+package com.hangzhou.jin.customview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Jin on 2016/7/8.
+ */
+public class TailsTextView extends TextView {
+
+	private String leftTxt = "";
+	private String rightTxt = "";
+	private String innerTxt = "";
+	private boolean lineThrough = false;
+
+	public TailsTextView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+
+		TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TailsTextView);
+		try {
+			leftTxt = a.getString(R.styleable.TailsTextView_leftTextTails);
+			rightTxt = a.getString(R.styleable.TailsTextView_rightTextTails);
+			lineThrough = a.getBoolean(R.styleable.TailsTextView_line_through, false);
+		} finally {
+			a.recycle();
+		}
+
+		if (null == leftTxt)
+			leftTxt = "";
+		if (null == rightTxt)
+			rightTxt = "";
+
+		innerTxt = getText().toString();
+		this.setText(innerTxt + "");
+
+		if (lineThrough) {
+			this.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+		}
+	}
+
+	public void setText(String str) {
+		innerTxt = str;
+		super.setText(leftTxt + str + rightTxt);
+	}
+
+	public void setLeftTxt(String str) {
+		this.leftTxt = str;
+		setText(innerTxt);
+	}
+
+	public void setRightTxt(String str) {
+		this.rightTxt = str;
+		setText(innerTxt);
+	}
+}
---
+++ custom-view/src/main/java/com/hangzhou/jin/customview/TextMoreLayout.java
@@ -0,0 +1,54 @@
+package com.hangzhou.jin.customview;
+
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Jin on 2016/7/4.
+ * Description
+ */
+public class TextMoreLayout extends RelativeLayout {
+
+	TextView leftText;
+	TextView rightText;
+
+	public TextMoreLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+
+		TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TextMoreLayout);
+		try {
+			String leftTxt = a.getString(R.styleable.TextMoreLayout_tmLeftText);
+			String rightTxt = a.getString(R.styleable.TextMoreLayout_tmRightTextTails);
+			boolean rightImgVisible = a.getBoolean(R.styleable.TextMoreLayout_tmRightImgVisible, true);
+
+			View view = View.inflate(context, R.layout.text_more_layout, null);
+
+			leftText = (TextView) view.findViewById(R.id.leftText);
+			if (null != leftTxt)
+				leftText.setText(leftTxt);
+			rightText = (TextView) view.findViewById(R.id.rightText);
+			if (null != rightTxt)
+				rightText.setText(rightTxt);
+			if (!rightImgVisible)
+				view.findViewById(R.id.more).setVisibility(View.GONE);
+
+			this.addView(view);
+		} finally {
+			a.recycle();
+		}
+	}
+
+	public void setLeftText(String str) {
+		leftText.setText(str);
+	}
+
+	public void setRightText(String str) {
+		rightText.setText(str);
+	}
+
+}
