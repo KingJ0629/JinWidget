4007b2b86bff964a8e16574e75222dff632ee120
金锋 <jinfeng@uama.com.cn>
2016-07-26 19:31:56 +0800

    textMoreView set text color & fix bugs

--- custom-view/src/main/java/com/hangzhou/jin/customview/TextMoreLayout.java
+++ custom-view/src/main/java/com/hangzhou/jin/customview/TextMoreLayout.java
@@ -3,6 +3,7 @@ package com.hangzhou.jin.customview;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -27,14 +28,22 @@ public class TextMoreLayout extends RelativeLayout {
 			int leftTextColor = a.getColor(R.styleable.TextMoreLayout_tmLeftTextColor, context.getResources().getColor(R.color.tm_default_left_text_color));
 			int rightTextColor = a.getColor(R.styleable.TextMoreLayout_tmRightTextColor, context.getResources().getColor(R.color.tm_default_right_text_color));
 
+			int defaultLeftSize = getResources().getDimensionPixelSize(R.dimen.text_more_layout_default_left_text_size);
+			float leftTextSize = a.getDimension(R.styleable.TextMoreLayout_tmLeftTextSize, defaultLeftSize);
+			int defaultRightSize = getResources().getDimensionPixelSize(R.dimen.text_more_layout_default_right_text_size);
+			float rightTextSize = a.getDimension(R.styleable.TextMoreLayout_tmRightTextSize, defaultRightSize);
+
 			View view = View.inflate(context, R.layout.text_more_layout, null);
 
 			leftText = (TextView) view.findViewById(R.id.leftText);
 			leftText.setTextColor(leftTextColor);
+			leftText.setTextSize(TypedValue.COMPLEX_UNIT_PX, leftTextSize);
 			if (null != leftTxt)
 				leftText.setText(leftTxt);
+
 			rightText = (TextView) view.findViewById(R.id.rightText);
 			rightText.setTextColor(rightTextColor);
+			rightText.setTextSize(TypedValue.COMPLEX_UNIT_PX, rightTextSize);
 			if (null != rightTxt)
 				rightText.setText(rightTxt);
 			if (!rightImgVisible)
